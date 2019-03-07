78cb9fdc8d13bee2fbe06e201d86dfaabb4a3fc6
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-07 19:38:45 +0000

    first unit test passing for Thermostat initialization at 20 degrees

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,14 @@
+// 'use strict';
+//
+// describe('Feature Tests', function(){
+// var temp;
+//
+// beforeEach(function(){
+//   temp = 20;
+// });
+//
+// it('initializes with temperature of 20 degC', function(){
+//   expect(Thermostat.tem()).toEqual(20);
+// });
+//
+// });
