3208646afc5c056a038380ec90e614f63a096b2a
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-07 19:48:41 +0000

    passing unit test for returning current temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,7 +8,8 @@ thermostat = new Thermostat
 });
 
 it('initializes with tempertaure of 20 degC', function(){
-expect(thermostat.temperature).toEqual(20);
+//use brackets at end to tell JS that we want to execute this function instead of returning function
+expect(thermostat.getCurrentTemperature()).toEqual(20);
 });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,4 +2,14 @@
 
 function Thermostat(){
 this.temperature = 20;
+
+
+Thermostat.prototype.getCurrentTemperature = function(){
+  return this.temperature;
 };
+
+};
+// things to notice:
+// 1. temperature property is in constructor function, between {}
+// 2. this keyword is used to make temperature an attribute of Thermostat
+// 3. need to use getter method so that we cannot get access to a variable outside Thermostat object -- go to ThermostatSpec and use method instead of attribute
